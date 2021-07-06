79de87f132de32102e9b519ddf289097bb429909
panggggg <p.picczy@gmail.com>
2021-07-06 17:59:22 +0700

    Add new route

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,12 +1,56 @@
 const express = require("express");
+const { JsonWebTokenError } = require("jsonwebtoken");
 const app = express();
 const mongoose = require('mongoose');
+const User = require('./user');
 
 mongoose.connect("mongodb://localhost/auth-service", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, () => {
     console.log("Auth-Service DB Connected");
+});
+
+app.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.json({ message: "User doesn't exits" });
+    } else {
+
+        //Check if the entered password is valid
+        if (password !== user.password) {
+            return res.json({ message: "Password Incorrect" });
+        }
+        const payload = {
+            email,
+            name: user.name
+        };
+        jwt.sign(payload, "secret", (err, token) => {
+            if (err) consoloe.log(err);
+            else {
+                return res.json({ token: token });
+            }
+        });
+    }
+});
+
+app.post("/auth/register", async (req, res) => {
+    const { email, password, name } = req.body;
+
+    const userExits = await User.findOne({ email });
+    if (userExits !== null) {
+        return res.json({ message: "User already exits" });
+    } else {
+        const newUser = new User({
+            name,
+            email,
+            password
+        });
+        newUser.save();
+        return res.json(newUser);
+    }
 });
 
 app.use(express.json());
