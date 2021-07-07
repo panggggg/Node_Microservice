51a1c33a8f2050a95a6dc9bd0aef37940c2db774
panggggg <p.picczy@gmail.com>
2021-07-07 17:55:39 +0700

    Add product route

--- product-service/index.js
+++ product-service/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
 const mongoose = require('mongoose');
-// const User = require('./user');
+const Product = require('./product');
 const amqp = require("amqplib/callback_api");
+const isAuthenticated = require('../isAuthenticated');
 
 app.use(express.json());
 
@@ -25,6 +26,19 @@ amqp.connect('amqp://localhost:5672', (connErr, connection) => {
 
         channel.assertQueue("PRODUCT");
     });
+});
+
+//Create a new product.
+//Buy a product.
+
+app.post("/product/create", isAuthenticated, async (req, res) => {
+    const { name, description, price } = req.body;
+    const newProduct = new Product({
+        name,
+        description,
+        price,
+    });
+    return res.json(newProduct);
 });
 
 app.listen(8080, () => {
