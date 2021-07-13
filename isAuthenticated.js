1d9c445b354e8ce81009a519b48da0bc8de50ca1
panggggg <p.picczy@gmail.com>
2021-07-13 16:50:51 +0700

    Edit authorization

--- isAuthenticated.js
+++ isAuthenticated.js
@@ -2,8 +2,14 @@ const jwt = require("jsonwebtoken");
 
 //Middleware
 
-export async function isAuthenticated(req, res, next) {
-    const token = req.header["authorization"].split(" ")[1];
+module.exports = async function isAuthenticated(req, res, next) {
+    if (typeof req.headers.authorization !== 'string') {
+        res.sendStatus(400);
+        return;
+    }
+    const token = req.headers.authorization.split(" ")[1];
+    //"Bearer <token>".split(" ")[1];
+    //"Bearer", "<token>"
 
     jwt.verify(token, "secret", (err, user) => {
         if (err) {
